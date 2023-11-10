ffcbc4a0a4507b639f890967a096bb888a72debe
Rafiw Idrissu <dev.rafiu@gmail.com>
2023-11-10 09:39:55 -0800

    feat: implement updating of invoice

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -5,7 +5,7 @@ import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
-const invoiceSchema = z.object({
+const InvoiceSchema = z.object({
   id: z.string(),
   customerId: z.string(),
   amount: z.coerce.number(),
@@ -13,7 +13,7 @@ const invoiceSchema = z.object({
   date: z.string(),
 });
 
-const CreateInvoice = invoiceSchema.omit({ id: true, date: true });
+const CreateInvoice = InvoiceSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
@@ -28,6 +28,30 @@ export async function createInvoice(formData: FormData) {
   await sql`
        INSERT INTO invoices (customer_id, amount, status, date)
        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+// Use Zod to update the expected types
+const UpdateInvoice = InvoiceSchema.omit({ date: true, id: true });
+
+// ...
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
   `;
 
   revalidatePath("/dashboard/invoices");
